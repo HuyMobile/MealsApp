8f7b0f7540807a70d52c282806931728e418b8b3
huy <huyvqmobile@gmail.com>
2023-02-20 10:50:33 +0700

    navigating to the new page

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,3 +1,4 @@
+import 'package:appmeals/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 
@@ -5,18 +6,28 @@ class CategoriesItem extends StatelessWidget {
 final String title;
 final Color color;
 CategoriesItem(this.title,this.color);
+
+void selectCategory(BuildContext ctx){
+  Navigator.of(ctx).push(MaterialPageRoute(builder: (_)=>
+  CategoryMealsScreen()));
+}
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title,style: Theme.of(context).textTheme.titleMedium,),
-      decoration: BoxDecoration(gradient:LinearGradient(
-        colors: [color.withOpacity(0.7),
-        color,],
-      begin:Alignment.topLeft,
-      end:Alignment.bottomRight ),
-        borderRadius: BorderRadius.circular(15),
+    return InkWell(
+      onTap:()=> selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title,style: Theme.of(context).textTheme.titleMedium,),
+        decoration: BoxDecoration(gradient:LinearGradient(
+          colors: [color.withOpacity(0.7),
+          color,],
+        begin:Alignment.topLeft,
+        end:Alignment.bottomRight ),
+          borderRadius: BorderRadius.circular(15),
 
+        ),
       ),
     );
   }
